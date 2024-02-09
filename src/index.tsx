9710f675f3ee2b44eba08ac2d68372b107a51bf0
Martin Began <martin.began@kiwi.com>
2024-02-09 17:52:47 +0100

    chore: updates to Client Rendering APIs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import {ChakraProvider} from '@chakra-ui/react';
 import {StrictMode} from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import {HelmetProvider} from 'react-helmet-async';
 import App from './App';
 import GlobalStyles from './GlobalStyles';
@@ -10,7 +10,7 @@ import theme from './theme';
 
 const MOUNT_NODE = document.getElementById('root');
 
-ReactDOM.render(
+createRoot(MOUNT_NODE!).render(
   <StrictMode>
     <ChakraProvider theme={theme} resetCSS>
       <HelmetProvider>
@@ -19,6 +19,5 @@ ReactDOM.render(
         <WebVitals showStatusInConsoleLog />
       </HelmetProvider>
     </ChakraProvider>
-  </StrictMode>,
-  MOUNT_NODE
+  </StrictMode>
 );
