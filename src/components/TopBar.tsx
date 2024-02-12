4b9db5adba945331817954304af69ccefcb6cd7e
Martin Began <martin.began@kiwi.com>
2024-02-12 11:01:56 +0100

    feat: adds Link to the logo in TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,7 @@
 import {useTranslation} from 'react-i18next';
-import {useLocation} from 'react-router-dom';
+import {useLocation, Link as ReactRouterLink} from 'react-router-dom';
 import {useStore} from '@nanostores/react';
-import {Flex, Heading, HStack, Image, Spacer, Text} from '@chakra-ui/react';
+import {Flex, Heading, HStack, Image, Link, Spacer, Text} from '@chakra-ui/react';
 
 import UserMenu from './UserMenu';
 import logo from '../assets/logo.svg';
@@ -14,15 +14,24 @@ const TopBar = () => {
   const user = useStore($user);
   const name = user ? `${user.firstName} ${user.lastName}` : MISSING_TEXT;
 
+  const isLoginPage = pathname === '/login';
+
   return (
     <Flex h={{base: 'topbar.base', md: 'topbar.md'}} alignItems="center">
-      <HStack margin="auto">
-        <Image boxSize="topbar.logo-boxSize" src={logo} alt="Logo" />
-        <Heading color="fill-darkBlue" size="sm">
-          {t('app.name')}
-        </Heading>
-      </HStack>
-      {pathname !== '/login' && (
+      <Link
+        as={ReactRouterLink}
+        to={isLoginPage ? '/login' : '/overview'}
+        _hover={{textDecoration: 'none'}}
+        margin="auto"
+      >
+        <HStack>
+          <Image boxSize="topbar.logo-boxSize" src={logo} alt="Logo" />
+          <Heading color="fill-darkBlue" size="sm">
+            {t('app.name')}
+          </Heading>
+        </HStack>
+      </Link>
+      {!isLoginPage && (
         <>
           <Spacer />
           <UserMenu>
